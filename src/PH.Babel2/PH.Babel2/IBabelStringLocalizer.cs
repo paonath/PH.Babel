efa0c1da2628da5d0f2089eb8fbb4eb9a1c9dc1b
DESKTOP-KF2J20I\paolo <paolo.innocenti@estrobit.com>
2019-01-03 16:24:17 +0100

    fix ambiguity error between interfaces v.1.0.2

--- src/PH.Babel2/PH.Babel2/IBabelStringLocalizer.cs
+++ src/PH.Babel2/PH.Babel2/IBabelStringLocalizer.cs
@@ -1,14 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using Microsoft.Extensions.Localization;
 
 namespace PH.Babel2
 {
     public interface IBabelStringLocalizer : IBabel, IStringLocalizer
     {
+        
 
+        new BabelLocalizedString this[string name] { get; }
+        new BabelLocalizedString this[CultureInfo culture, string name] { get; }
+        new BabelLocalizedString this[Type resourceType,CultureInfo culture, string name] { get; }
+        
+
+        new BabelLocalizedString this[string name, params object[] arguments] { get; }
+        new BabelLocalizedString this[CultureInfo culture, string name, params object[] arguments] { get; }
+        new BabelLocalizedString this[Type resourceType,CultureInfo culture, string name, params object[] arguments] { get; }
     }
 
     public interface IBabelStringLocalizer<TResource> : IBabel, IStringLocalizer<TResource>
     {
+        new BabelLocalizedString<TResource> this[string name] { get; }
+        new BabelLocalizedString<TResource> this[CultureInfo culture, string name] { get; }
+        new BabelLocalizedString<TResource> this[Type resourceType,CultureInfo culture, string name] { get; }
+        
 
+        new BabelLocalizedString<TResource> this[string name, params object[] arguments] { get; }
+        new BabelLocalizedString<TResource> this[CultureInfo culture, string name, params object[] arguments] { get; }
+        new BabelLocalizedString<TResource> this[Type resourceType,CultureInfo culture, string name, params object[] arguments] { get; }
     }
 }
